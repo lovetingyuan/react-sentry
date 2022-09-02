93c1a745ffcba9d4f76964da43e6125c68ad1cfd
黄百途 <hbtwhucsis@gmail.com>
2022-09-02 17:08:17 +0800

    chore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './style.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
 
+const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
+
+// App.js
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz and Sentry!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <SentryRoutes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </SentryRoutes>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from 'react-router-dom';
 
 import App from './App';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+Sentry.init({
+  dsn: 'https://73df6f3bf6304f518d809cc18f287daa@o58488.ingest.sentry.io/6711675',
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+      ),
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
